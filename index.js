de05c23a1aee770df8c1fea09bc5899080b022e3
dalal aljassem <deealjassem@dalals-MacBook-Pro.local>
2022-03-27 22:43:14 +0300

    done

--- index.js
+++ index.js
@@ -22,7 +22,7 @@
  * 7. (1000 Points!)- great now you noticed that you created 4 objects manually, but that shouldn't be the case.
  *      the Uno deck includes 80 numbers (0-9 from each color)
  *
- *      it has four colors ["Yellow","Red","Blue","Green"]
+ *      it has four colors ["Yellow","Red","Blue","Green"] colors length = 4
  *    - Comment out the objects that you created
  *    - you should fill the cards dynamically, instead of the ones we created object by object,
  *      you need to use loops and fill the cards deck in a loop (Hint: You should use `for` loops, )
@@ -32,11 +32,44 @@ let cards = []; // You will use this array in step 6
 
 // Continue the code here 👇🏻
 
+class Card{
+  constructor(Number,color){
+  this.Number = Number;
+  this.color = color;
+  }
+
+  image(){
+    return `./images/${this.color}_${this.Number}.png`;
+  }
+}
+  // let cardB = new Card(0,"Blue");
+  // let cardG = new Card(1,"Green");
+  // let cardR = new Card(2,"Red");
+  // let cardY = new Card(3,"Yellow");
+  // console.log(cardB);
+  // console.log(cardG);
+  // console.log(cardR);
+  // console.log(cardY);
+  // cards.push(cardB);
+  // cards.push(cardG);
+  // cards.push(cardR);
+  // cards.push(cardY);
+
+  const colors=["Blue","Red","Green","Yellow"];
+  colors.forEach(c => {
+    for (let i = 0; i <= 9; i++){
+    let card = new Card(i,c);
+    cards.push(card); }
+  });
+
+//looping through colors array , blue green red yellow
+//looping through numbers array , till 10
 /**
  *
  * Don't touch this code
  * This function is going to render the cards array to the HTML page,
  */
+
 function render(array) {
   document.getElementById("cards-container").innerHTML = array
     .map(cardComponent)
